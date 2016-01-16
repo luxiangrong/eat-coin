8c5a60d66c9970adc76e3ebeceeb64f36dabd03d
LuXiangrong <luxiangrongwave@gmail.com>
2016-01-16 23:44:42 +0800

    游戏页一半

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,6 +21,10 @@ Pace.on('done', function() {
             (function(ARE) {
                 var w = $(document).width();
                 var h = $(document).height();
+
+                var cdTime = 20 * 1000;
+                var gameStart = false;
+
                 $('canvas#game').prop('width', w).prop('height', h);
                 var Stage = ARE.Stage,
                     Bitmap = ARE.Bitmap,
@@ -32,31 +36,180 @@ Pace.on('done', function() {
                 var ld = new Loader(),
                     cloud1, cloud2, cloud3, cloud4, cloud5, cloud6;
                 var stage = new Stage("#game", localStorage.webgl == "1");
+                stage.debug = true;
 
-                //初始化彩色纸片
+
+                /**-------------------- 游戏开始倒计时 start -------------------**/
+
+                var ldCD = new Loader();
+                ldCD.loadRes([{
+                    id: "cd-1",
+                    src: "images/p26.png"
+                }, {
+                    id: "cd-2",
+                    src: "images/p27.png"
+                }, {
+                    id: "cd-3",
+                    src: "images/p28.png"
+                }, {
+                    id: "cd-go",
+                    src: "images/p29.png"
+                }]);
+
+                ldCD.complete(function() {
+                    var mask = new RectShape(w, h, 'black', false);
+                    mask.alpha = 0.5;
+                    mask.x = w / 2;
+                    mask.y = h / 2;
+                    stage.add(mask);
+                    var cd = new Bitmap(ldCD.get("cd-3"));
+                    cd.originX = cd.originY = 0.5;
+                    cd.x = w / 2;
+                    cd.y = h / 2;
+                    cd.scaleX = cd.scaleY = 0;
+                    cd.alpha = 0;
+                    To.get(cd)
+                        .to()
+                        .scaleX(0.5, 1000, To.elasticOut)
+                        .scaleY(0.5, 1000, To.elasticOut)
+                        .alpha(1, 500)
+                        .end(function() {
+                            cd.useImage(ldCD.get("cd-2"));
+                            cd.scaleX = cd.scaleY = 0;
+                            cd.alpha = 0;
+                        })
+                        .to()
+                        .scaleX(0.5, 1000, To.elasticOut)
+                        .scaleY(0.5, 1000, To.elasticOut)
+                        .alpha(1, 500)
+                        .end(function() {
+                            cd.useImage(ldCD.get("cd-1"));
+                            cd.scaleX = cd.scaleY = 0;
+                            cd.alpha = 0;
+                        })
+                        .to()
+                        .scaleX(0.5, 1000, To.elasticOut)
+                        .scaleY(0.5, 1000, To.elasticOut)
+                        .alpha(1, 500)
+                        .end(function() {
+                            cd.useImage(ldCD.get("cd-go"));
+                            cd.scaleX = cd.scaleY = 0;
+                            cd.alpha = 0;
+                        })
+                        .to()
+                        .scaleX(0.5, 1000, To.elasticOut)
+                        .scaleY(0.5, 1000, To.elasticOut)
+                        .alpha(1, 500)
+                        .end(function() {
+                            stage.remove(cd);
+                            stage.remove(mask);
+                            gameStart = true;
+                            stage.remove(paperContainer);
+                        })
+                        .start();
+
+                    stage.add(cd);
+
+                });
+                /**-------------------- 游戏开始倒计时 end -------------------**/
+
+                /**-------------------- 金币下落 start -------------------**/
+                var startPerSecond = 1; // 开始时每秒掉落金币数
+                var endPerSecond = 3; //结束时每秒掉落金币数
+                var totalSeconde = 20000; //游戏总时间(毫秒)
+                var loadCoin = new Loader();
+                var intervalTime = function(a, b, c, t) {
+                    var r = (1000 * (a - b) / (c * b * a) * t + 1000 / a);
+                    if( r < 1000 / b) {
+                        return 1000 / b;
+                    }
+                    return r;
+                }
+                loadCoin.loadRes([{
+                    id: "gold",
+                    src: "images/p30.png"
+                }, {
+                    id: "silver",
+                    src: "images/p31.png"
+                }, {
+                    id: "copper",
+                    src: "images/p32.png"
+                }]);
+                loadCoin.complete(function() {
+                    var startTime = new Date().valueOf();
+                    var lastTime = new Date().valueOf();
+                    var xArray = [ w / 5, w / 2, w / 5 * 4];
+                    var coinContainer = new ARE.Container();
+                    stage.add(coinContainer);
+                    stage.onTick(function() {
+                        if (!gameStart) return;
+                        var nowTime = new Date().valueOf();
+                        if(nowTime - lastTime >  intervalTime(startPerSecond, endPerSecond, totalSeconde, nowTime - startTime)) {    
+                             var r = _.random(0,2);
+                            var coin;
+                            switch(r) {
+                                case 0:
+                                    coin = new Bitmap(loadCoin.get('gold'));
+                                    break;
+                                case 1:
+                                    coin = new Bitmap(loadCoin.get('silver'));
+                                    break;
+                                case 2:
+                                    coin = new Bitmap(loadCoin.get('copper'));
+                                    break;
+                            }
+                            coin.originX = coin.orginY = 0.5
+                            coin.scaleX = coin.scaleY = 0.5;
+                            coin.x = xArray[_.random(0,2)];
+                            To.get(coin)
+                                .to()
+                                .y(h, intervalTime(0.333, 0.75, totalSeconde, nowTime - startTime))
+                                .end(function(){
+                                    coinContainer.remove(coin);
+                                })
+                                .start();
+                            coinContainer.add(coin);
+                            lastTime = nowTime;
+                        }
+                    })
+                });
+                /**-------------------- 金币下落 end -------------------**/
+
+
+                /**-------------------- 初始化彩色纸片 start -------------------**/
                 var colorPaperArr = [];
-                for(var i = 1; i < 10; i ++) {
-                    var colorPaper = new RectShape(16,16,'white',false);
+                for (var i = 1; i < 10; i++) {
+                    var colorPaper = new RectShape(16, 16, 'white', false);
                     colorPaper.x = w / 10 * i;
-                    colorPaper.y = h / 10 * i;
+                    colorPaper.y = 0;
                     colorPaper.scaleX = colorPaper.scaleY = _.random(5, 10) / 10;
-                    colorPaper.setFilter(_.random(0, 10)/10, _.random(0, 10)/10, _.random(0, 10)/10, _.random(5, 8)/10);
+                    colorPaper.setFilter(_.random(0, 10) / 10, _.random(0, 10) / 10, _.random(0, 10) / 10, _.random(5, 8) / 10);
                     colorPaperArr.push(colorPaper);
                 }
+                var paperContainer = new ARE.Container();
+                stage.add(paperContainer);
+                //彩色纸片下落动画
+                var createColorPaperTween = function(ele) {
+                    var duration = _.random(1000, 2500);
+                    To.get(ele)
+                        .to()
+                        .set("y", h, duration)
+                        .rotation(1080, duration)
+                        .to()
+                        .set("y", 0, 0)
+                        .rotation(0, 0)
+                        .end(function() {
+                            ele.setFilter(_.random(0, 10) / 10, _.random(0, 10) / 10, _.random(0, 10) / 10, _.random(5, 8) / 10);
+                            createColorPaperTween(ele);
+                        }).start();
+                };
+                _.each(colorPaperArr, function(paper) {
+                    createColorPaperTween(paper);
+                    paperContainer.add(paper);
+                });
+                /**-------------------- 初始化彩色纸片 end -------------------**/
 
-                var colorPaperTween = To.get(colorPaperArr[0])
-                   .cycle(0,10) 
-                   .to()
-                   .set("y", 240, 2000, To.elasticInOut)
-                   .rotation(240, 2000, To.elasticInOut)
-                   .end(function () {
-                       //console.log(" task one has completed!")
-                   })
-                   .to()
-                   .set("y", 0, 2000, To.elasticInOut) 
-                   .to()
-                   .start();
-                   
+
 
                 //载入云层资源
                 ld.loadRes([{
@@ -141,9 +294,7 @@ Pace.on('done', function() {
                     stage.add(cloud6);
                     stage.add(cloud4);
 
-                    _.each(colorPaperArr, function(paper){
-                        stage.add(paper);
-                    });
+
 
                     var step = 0.01;
                     //定义云层的运动速度和范围
@@ -180,7 +331,7 @@ Pace.on('done', function() {
                     }];
                     //在舞台的循环事件中定义运动逻辑
                     stage.onTick(function() {
-                        
+
                         //云层的运动逻辑
                         for (var i = 0; i < clouds.length; i++) {
                             if (clouds[i].x <= steps[i].xRange[0] || clouds[i].x >= steps[i].xRange[1]) {
@@ -199,10 +350,10 @@ Pace.on('done', function() {
         }
 
         var swiper = new Swiper('.swiper-container', {
-            direction: 'vertical',
-            effect: 'fade',
+            // direction: 'vertical',
+            // effect: 'fade',
             preventClicks: false,
-            onlyExternal: true,
+            // onlyExternal: true,
             onSlideChangeEnd: function(swiper) {
                 swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
                 var activeIndex = swiper.activeIndex;
@@ -212,6 +363,7 @@ Pace.on('done', function() {
                         addAnimation();
                         break;
                     case 1:
+                        $('.swiper-slide-1 *').css('animation', 'none');
                         initGame();
                         break;
                 }
@@ -223,16 +375,15 @@ Pace.on('done', function() {
                 var activeIndex = swiper.activeIndex;
                 switch (activeIndex) {
                     case 0:
-                        {
-                            var fireworksArray = $('#tpl-fireworks').html();
-                            $('.fireworks-container').empty();
-                            for (var i = 0; i < 8; i++) {
-                                $('.fireworks-container').append(fireworksArray);
-                            }
-                            $('.fireworks-container .fireworks').each(function(index, item) {
-                                $(item).css('animation', 'falling-' + (index % 2 + 1) + ' ' + (Math.random() * 5 + 5) + 's linear both ' + (Math.random() * 10) + 's');
-                            });
+                        var fireworksArray = $('#tpl-fireworks').html();
+                        $('.fireworks-container').empty();
+                        for (var i = 0; i < 8; i++) {
+                            $('.fireworks-container').append(fireworksArray);
                         }
+                        $('.fireworks-container .fireworks').each(function(index, item) {
+                            $(item).css('animation', 'falling-' + (index % 2 + 1) + ' ' + (Math.random() * 5 + 5) + 's linear both ' + (Math.random() * 10) + 's');
+                        });
+                        break;
                 }
             }
         });
@@ -261,5 +412,3 @@ Pace.on('done', function() {
 
     });
 });
-
-
